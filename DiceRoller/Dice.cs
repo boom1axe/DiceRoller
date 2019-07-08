b4f129fa64fd11a596f03b624f97106027fc4ff2
Dennis Kafton <boom1axe@att.net>
2019-07-08 13:32:58 -0700

    Added basic die class

---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    /// <summary>
+    /// Repersents a single
+    /// multi-sided die
+    /// </summary>
+    class Dice
+    {
+        private byte faceValue;
+        private bool isHeld;
+        private byte numbersOfSides;
+
+        public byte Roll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
